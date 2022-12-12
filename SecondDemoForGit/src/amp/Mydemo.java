e096ec870ad82109b317c6e1277adc0677989798
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2022-12-12 11:42:23 +0530

    Experiment Trials Going On

---
+++ SecondDemoForGit/src/amp/Mydemo.java
@@ -0,0 +1,10 @@
+package amp;
+
+public class Mydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Trial Successfull");
+	}
+
+}
---
+++ SecondDemoForGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Arshad Patel
+ *
+ */
+module SecondDemoForGit {
+}
